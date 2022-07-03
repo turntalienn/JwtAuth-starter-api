f1dbd6ba608f22010d1f8e980fa5fdd43f4c5987
Ajmal <75809632+turntalienn@users.noreply.github.com>
2022-07-03 16:57:44 +0530

    Added files via upload

---
+++ src/main/java/com/turntalien/jwtsecurity/JwtSecurity.java
@@ -0,0 +1,13 @@
+package com.turntalien.jwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurity {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurity.class, args);
+	}
+
+}
